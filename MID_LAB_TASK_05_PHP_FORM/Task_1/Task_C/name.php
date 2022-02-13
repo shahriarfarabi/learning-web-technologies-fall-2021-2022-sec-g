22ce3af426396ecd63c937b70d2bc216b5546727
shahriarfarabi <shahriarfarabi1998@gmail.com>
2022-02-13 11:52:02 +0600

    MID_LAB_TASK_05_PHP_FORM

---
+++ MID_LAB_TASK_05_PHP_FORM/Task_1/Task_C/name.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+	<title>Name input</title>
+</head>
+<body>
+
+	<form method="POST" action="">
+		<fieldset>
+			<legend>NAME</lebgend>
+				<input type="text" name="myname" value="<?php if(isset($_POST['myname'])){ echo $_POST['myname']; }?>">  <br>
+				<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
